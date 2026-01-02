5df6fb1c66e3c63ec8833b26055795e1d849c774
aasif-create <aasifmmd12345@gmail.com>
2026-01-02 23:38:07 +0530

    practiced leetcode problem #9

---
+++ PalindromeNumber.java
@@ -0,0 +1,39 @@
+package leetcode26;
+import java.util.Scanner;
+public class PalindromeNumber
+{
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        int temp = x;
+        int rev = 0;
+        if (x < 0)
+        {
+            System.out.println("Not a Palindrome");
+        }
+        else
+        {
+            while (x > 0)
+            {
+                rev = rev * 10 + (x % 10);
+                x = x / 10;
+            }
+            if (temp == rev)
+                System.out.println("Palindrome");
+            else
+                System.out.println("Not a Palindrome");
+        }
+        sc.close();
+    }
+}
+/* EXPLANATION :
+1. We take a number as input from the user.
+2. We store the original number in a temporary variable.
+3. If the number is negative, it cannot be a palindrome.
+4. We reverse the number using a while loop.
+5. We compare the reversed number with the original number.
+6. If both are equal, the number is a palindrome, otherwise it is not.
+*/
---
+++ TwoSum.java
@@ -0,0 +1,32 @@
+package leetcode26;
+public class TwoSum
+{
+    public static int[] twoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args)
+    {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indexes: " + result[0] + ", " + result[1]);
+    }
+}
+/* EXPLANATION :
+1. We are given an array of numbers and a target value.
+2. We check each number with every other number.
+3. If the sum of two numbers equals the target, we stop.
+4. We return the positions (indexes) of those two numbers.
+5. We use two loops to compare all possible pairs.
+*/
