1494d27854aea27a880dfa6f5caeba2097e7563d
aasif-create <aasifmmd12345@gmail.com>
2026-02-27 20:43:31 +0530

    PRACTICED LEETCODE PROBLEM #58 FINDING THE LENGTH OF THE LAST WORD OF THE GIVEN STRING

---
+++ LengthOfLastWord.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int count = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+        int result = lengthOfLastWord(s);
+        System.out.println("Length of last word: " + result);
+        sc.close();
+    }
+}
